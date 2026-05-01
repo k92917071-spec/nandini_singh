f07ce713136ccc48d5cb82ce343d5767497bf14e
k92917071-spec <k92917071@gmail.com>
2026-05-01 07:34:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function showMessage() {
+    document.getElementById("message").style.display = "block";
+}
+
+function quizAnswer() {
+    alert("Bilkul sahi 😄 Tumhari priority hi tumhe special banati hai!");
+}
